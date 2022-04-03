c1d129af9d456feaba37602cb5eadaf848e7fcf8
3proxy  production environment <raintears2@qq.com>
2022-04-03 23:59:02 +0800

    Update checkbandwidth.sh

--- checkbandwidth.sh
+++ checkbandwidth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /etc/profile
 PATH=/usr/sbin:/usr/local/bin:$PATH
-aws lightsail get-regions|jq .regions[].name|xargs -i aws lightsail --region {} get-instances |jq .instances[]|jq -r '"\(.name) \(.publicIpAddress)"' |xargs -i -P 10 sh -c "sh /root/shutdownbandwidth.sh {}"
+aws lightsail get-regions|jq .regions[].name|xargs -i aws lightsail --region {} get-instances |jq .instances[]|jq -r '"\(.name) \(.publicIpAddress) \(.location.regionName)"' |xargs -i -P 10 sh -c "sh shutdownbandwidth.sh {}"
